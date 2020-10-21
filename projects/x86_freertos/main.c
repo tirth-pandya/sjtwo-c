103783579cf4db89967971a5c6dc5a6bedc3fca4
Tirth_Pandya <tirth_pandya@yahoo.com>
2020-10-20 19:58:12 -0700

    Producer Sender Task on Simulator

--- projects/x86_freertos/main.c
+++ projects/x86_freertos/main.c
@@ -5,19 +5,33 @@
 
 #include "cpu_utilization_task.h"
 
-/**
- * This POSIX based FreeRTOS simulator is based on:
- * https://github.com/linvis/FreeRTOS-Sim
- *
- * Do not use for production!
- * There may be issues that need full validation of this project to make it production intent.
- * This is a great teaching tool though :)
- */
+#include "queue.h"
+
+static QueueHandle_t q;
+
+void sender_task(void *p) {
+  int sent_data = 0;
+  while (1) {
+    if (!xQueueSend(q, &sent_data, 0)) {
+      printf("Couldn't send to the queue\n");
+    }
+    sent_data++;
+    vTaskDelay(1000);
+  }
+}
+
+void receiver_task(void *p) {
+  int received_data;
+  while (1) {
+    xQueueReceive(q, &received_data, portMAX_DELAY);
+    printf("Received %d\n", received_data);
+  }
+}
+
 int main(void) {
-  xTaskCreate(cpu_utilization_print_task, "cpu", 1, NULL, PRIORITY_LOW, NULL);
-
-  puts("Starting FreeRTOS Scheduler ..... \r\n");
+  q = xQueueCreate(10, sizeof(int));
+  xTaskCreate(sender_task, "sender", 1024, NULL, PRIORITY_LOW, NULL);
+  xTaskCreate(receiver_task, "receiver", 1024, NULL, PRIORITY_LOW, NULL);
   vTaskStartScheduler();
-
   return 0;
 }
